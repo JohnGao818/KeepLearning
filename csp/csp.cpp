fb804aabd1e82d79c7bcc9905eab5b287ebc179e
John <952392783@qq.com>
2021-10-16 12:44:34 +0800

    Add files via upload

---
+++ csp/csp.cpp
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdio.h>
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    long n;
+    cin>>n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin>>a[i];
+    }
+    
+    system("pause");
+    return 0;
+}
