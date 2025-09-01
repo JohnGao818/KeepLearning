745cdada4a53442a17ccc2b4d0e07e7bee4467b1
john <952392783@qq.com>
2025-09-01 21:21:13 +0800

    feat: wincpp

---
+++ wangdao/wind.cpp
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <cmath>
+
+#define MaxSize 50
+// 顺序表
+typedef struct SeqList
+{
+    int data[MaxSize];
+    int length;
+} SeqList;
+
+void InitList(SeqList &L)
+{
+    L.length = 0;
+}
+
+int Length(SeqList L)
+{
+    return L.length;
+}
+
+int LocateElem(SeqList L, int e)
+{
+    for (int i = 0; i < L.length; i++)
+        if (L.data[i] == e)
+            return i + 1;
+    return 0;
+}
+
+int GetElem(SeqList L, int i)
+{
+    return L.data[i];
+}
+
+bool ListInsert(SeqList &L, int i, int e)
+{
+    if (i < 1 || i > L.length + 1)
+        return false;
+    if (L.length >= MaxSize)
+        return false;
+    for (int j = L.length; j >= i; j--)
+        L.data[j] = L.data[j - 1];
+    L.data[i - 1] = e;
+    L.length++;
+    return true;
+}
+
+bool ListDelete(SeqList &L, int i, int &e)
+{
+    if (i < 1 || i > L.length + 1)
+        return false;
+    e = L.data[i - 1];
+    for (int j = i; j < L.length; j--)
+        L.data[j - 1] = L.data[j];
+    L.length--;
+    return true;
+}
+
+void PrintList(SeqList &L)
+{
+    for (int i = 0; i < L.length; i++) {
+        printf("%d, ", L.data[i]);
+    }
+    printf("\n");
+}
+
+bool Empty(SeqList L)
+{
+    return L.length == 0;
+}
+
+// 销毁线性表，释放线性表L所占用的内存空间
+void DestroyList(SeqList &L)
+{
+    L.length = 0;
+}
+
+void List_TailInsert(SeqList &L) {
+    int x;
+    scanf("%d", &x);
+    while (x != 9999)
+    {
+        L.data[L.length++] = x;
+        scanf("%d", &x);
+    }
+}
+
+int FindMinDistance(SeqList &L, SeqList &M, SeqList &N)
+{
+    int i = 0, j = 0, k = 0;
+    int temp;
+    int min = INT_MAX;
+    while (i < L.length && j < M.length && k < N.length)
+    {
+        temp = abs(L.data[i] - M.data[j]) + abs(L.data[i] - N.data[k]) + abs(M.data[j] - N.data[k]);
+        if (temp < min) {
+            min = temp;
+        }
+        if (L.data[i] < M.data[j] && L.data[i] < N.data[k]) {
+            i++;
+        } else if (M.data[j] < L.data[i] && M.data[j] < N.data[k]) {
+            j++;
+        } else {
+            k++;
+        }
+    }
+    return min;
+}
+
+int main()
+{
+    SeqList L;
+    InitList(L);
+    List_TailInsert(L);
+    PrintList(L);
+    return 0;
+}
